bed18649ea3dd28edea9aac75fa05518d4e6eec3
Pasquier Théo <psq.theo@gmail.com>
2022-11-03 15:04:11 +0100

    Initial commit

--- public/index.php
+++ public/index.php
@@ -1,3 +1,7 @@
 <?php
 
 require_once __DIR__ ."/../src/app.php";
+use Stable\Stable;
+
+$Stable1 = new Stable("Ecurie du soleil", "", "Rue de la marinière", "1346", "VilleImaginaire", "Antoine Daniel");
+echo $Stable1;
