cbb5c2e98af0fc49e2bdf43bfbfd88d2e3996b47
Pasquier Théo <psq.theo@gmail.com>
2022-11-03 17:11:23 +0100

    setting Id for Equine

--- public/index.php
+++ public/index.php
@@ -1,14 +1,21 @@
 <?php
 require_once __DIR__ . "/../src/app.php";
 
+use App\Model\Animal;
+use App\Model\Equine;
 use App\Model\Stable;
 use App\Model\Human;
 use App\Model\Manager;
-
-
+use App\Model\Rider;
 
 $manager1 = new Manager("Antoine Daniel", "France", "Rue Vincent Bray", "12317", "Braypolis", "???");
 
 $stable1 = new Stable("Ecurie du soleil", "France", "Rue de la marinière", "1346", "VilleImaginaire", $manager1);
 
-echo $stable1;
+// echo $stable1;
+
+$rider1 = new Rider("Antoine Daniel", "France", "Rue Vincent Bray", "12317", "Braypolis", "???");
+
+$equine1 = new Equine("Boby", "blue", 100, $rider1);
+
+echo $equine1;
